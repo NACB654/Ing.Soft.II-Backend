b843063b32c32d647ed4d6bc67839aa499985c20
NACB654 <nacb654@gmail.com>
2024-05-26 22:53:34 -0500

    Agregando unso scripts SQL

---
+++ lumen.investiga-backend/scripts/curso.sql
@@ -0,0 +1,4 @@
+INSERT INTO "Cursos" (descripcion, "creationDate", "updatedOn")
+VALUES ('Taller de Propuesta de Investigacion', NOW(), NOW()),
+('Seminario I', NOW(), NOW()),
+('Seminario II', NOW(), NOW()),
---
+++ lumen.investiga-backend/scripts/periodo.sql
@@ -0,0 +1,13 @@
+INSERT INTO "Periodos" (descripcion, "creationDate", "updatedOn")
+VALUES ('2023-2', NOW(), NOW()),
+('2023-1', NOW(), NOW()),
+('2023-0', NOW(), NOW()),
+('2022-2', NOW(), NOW()),
+('2022-1', NOW(), NOW()),
+('2022-0', NOW(), NOW()),
+('2021-2', NOW(), NOW()),
+('2021-1', NOW(), NOW()),
+('2021-0', NOW(), NOW()),
+('2020-2', NOW(), NOW()),
+('2020-1', NOW(), NOW()),
+('2020-0', NOW(), NOW()),
