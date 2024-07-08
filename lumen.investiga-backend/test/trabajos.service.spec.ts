28e7d33a2f52ef7b404370c1e56dada95fb82835
doliverosa <doa1602@gmail.com>
2024-07-07 20:42:56 -0500

    prueba unitaria visualizarTotales

--- lumen.investiga-backend/test/trabajos.service.spec.ts
+++ lumen.investiga-backend/test/trabajos.service.spec.ts
@@ -15,11 +15,11 @@ import { CursosService } from 'src/cursos/cursos.services';
 const mockTrabajoModel = { findAll: jest.fn() };
 const mockKeywordModel = {};
 const mockProfesoresService = {};
-const mockAsesoresService = {};
-const mockODSService = {};
-const mockPeriodosService = {};
-const mockSubareaService = {};
-const mockCursosService = {};
+const mockAsesoresService = { totalTrabajosAsesor: jest.fn() };
+const mockODSService = { totalTrabajosODS: jest.fn() };
+const mockPeriodosService = { totalTrabajosPeriodos: jest.fn() };
+const mockSubareaService = { totalTrabajosAreasSubareas: jest.fn() };
+const mockCursosService = { totalTrabajosCurso: jest.fn() };
 
 describe('TrabajosInvestigacionService', () => {
   let service: TrabajosInvestigacionService;
@@ -52,36 +52,67 @@ describe('TrabajosInvestigacionService', () => {
     expect(service).toBeDefined();
   });
 
-  describe('Mostrar resultados', () => {
-    it('debe retornar resultados que coincidan con la palabra clave', async () => {
-      const keyword = 'sostenibilidad';
-      const resultadosEsperados = [
-        { id: 1, titulo: 'Proyecto sobre sostenibilidad' },
-        { id: 2, titulo: 'Impacto ambiental y sostenibilidad' }
-      ];
-  
-      mockTrabajoModel.findAll.mockResolvedValue(resultadosEsperados);
-  
-      const result = await service.mostrarResultados(keyword);
-      expect(result).toEqual(resultadosEsperados);
+  it('debe retornar resultados que coincidan con la palabra clave', async () => {
+    const keyword = 'sostenibilidad';
+    const resultadosEsperados = [
+      { id: 1, titulo: 'Proyecto sobre sostenibilidad' },
+      { id: 2, titulo: 'Impacto ambiental y sostenibilidad' }
+    ];
+
+    mockTrabajoModel.findAll.mockResolvedValue(resultadosEsperados);
+
+    const result = await service.mostrarResultados(keyword);
+    expect(result).toEqual(resultadosEsperados);
+  });
+
+  it('debe retornar un array vacío si no hay coincidencias', async () => {
+    const keyword = 'inexistente';
+    mockTrabajoModel.findAll.mockResolvedValue([]);
+
+    const result = await service.mostrarResultados(keyword);
+    expect(result).toEqual([]);
+  });
+
+  it('debe manejar errores al buscar los resultados', async () => {
+    const keyword = 'error';
+    const error = new Error('Error al acceder a la base de datos');
+
+    mockTrabajoModel.findAll.mockRejectedValue(error);
+
+    const result = await service.mostrarResultados(keyword);
+    expect(result).toBeNull();
+  });
+
+
+  describe('Visualizar Totales', () => {
+    it('debe visualizar totales correctamente', async () => {
+      const totalesEsperados = {
+        asesores: 10,
+        ods: 5,
+        periodos: 7,
+        areasYSubareas: 3,
+        cursos: 8
+      };
+
+      mockAsesoresService.totalTrabajosAsesor.mockResolvedValue(10);
+      mockODSService.totalTrabajosODS.mockResolvedValue(5);
+      mockPeriodosService.totalTrabajosPeriodos.mockResolvedValue(7);
+      mockSubareaService.totalTrabajosAreasSubareas.mockResolvedValue(3);
+      mockCursosService.totalTrabajosCurso.mockResolvedValue(8);
+
+      const result = await service.visualizarTotales();
+      expect(result).toEqual(totalesEsperados);
     });
-  
-    it('debe retornar un array vacío si no hay coincidencias', async () => {
-      const keyword = 'inexistente';
-      mockTrabajoModel.findAll.mockResolvedValue([]);
-  
-      const result = await service.mostrarResultados(keyword);
-      expect(result).toEqual([]);
-    });
-  
-    it('debe manejar errores al buscar los resultados', async () => {
-      const keyword = 'error';
+
+    it('debe manejar errores al visualizar totales', async () => {
       const error = new Error('Error al acceder a la base de datos');
-    
-      mockTrabajoModel.findAll.mockRejectedValue(error);
-    
-      const result = await service.mostrarResultados(keyword);
+      mockAsesoresService.totalTrabajosAsesor.mockRejectedValue(error);
+
+      jest.spyOn(console, 'error').mockImplementation(() => { });
+
+      const result = await service.visualizarTotales();
       expect(result).toBeNull();
+      expect(console.error).toHaveBeenCalledWith(error);
     });
-  })
+  });
 });
